f10272a778fe877c3757e8dab1ab871a468dcd1f
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-04-18 19:10:45 +0200

    meals all params handling fixed

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -6,90 +6,84 @@ const knex = require("../database");
 
 router.get("/", async (request, response) => {
   try {
-    let queryToMeal = knex("meal");
+    let mealArrayToChange = await knex
+      .raw(
+        `SELECT meal.id,meal.title, meal.description, meal.location,meal.when_time, meal.price, meal.created_date,
+      meal.max_reservations,
+      COALESCE(SUM(reservation.number_of_guests),0) AS total_reserved
+      FROM   meal
+      LEFT JOIN reservation
+      ON meal.id = reservation.meal_id
+      GROUP  BY meal.id`
+      )
+      .then((result) => result[0]);
 
-    if (
-      typeof request.query["availableReservations"] !== "undefined" &&
-      request.query["availableReservations"] === "true"
-    ) {
-      // This way to make query not working in some reason. "Having" part does not act as expected
-      //
-      // queryToMeal = queryToMeal
-      //   .sum({ totalReserved: "reservation.number_of_guests" })
-      //   .join("reservation", "reservation.meal_id", "=", "meal.id")
-      //   .groupBy(
-      //     "meal.title",
-      //     "meal.description",
-      //     "meal.location",
-      //     "meal.when_time",
-      //     "meal.max_reservations",
-      //     "meal.price",
-      //     "meal.created_date"
-      //   )
-      //   .having("meal.max_reservations", ">", "totalReserved");     //act like ">=" not ">"
-      //
-      // This variant works, but with this way I can't handle several parameters in the same time
-      //
-      // const result =
-      //   await knex.raw(`SELECT meal.title, meal.description, meal.location,meal.when_time, meal.price, meal.created_date,
-      //   meal.max_reservations,
-      //   SUM(reservation.number_of_guests) AS reserved_meal
-      //   FROM   meal
-      //   JOIN reservation
-      //   ON meal.id = reservation.meal_id
-      //   GROUP  BY meal.title, meal.description, meal.location,meal.when_time, meal.price, meal.created_date,
-      //   meal.max_reservations
-      //   HAVING reserved_meal < meal.max_reservations`);
-      // if (result[0].length === 0) {
-      //   response.status(404).json({ message: "No meal available" });
-      // } else {
-      //   response.send(result[0]);
-      // }
-      // return;
+    if (Object.keys(request.query).length === 0) {
+      response.send(mealArrayToChange);
+      return;
     }
 
-    if (
-      request.query["maxPrice"] &&
-      !isNaN(Number(request.query["maxPrice"]))
-    ) {
-      queryToMeal = queryToMeal.where("price", "<", request.query["maxPrice"]);
+    if (typeof request.query["availableReservations"] !== "undefined") {
+      if (request.query["availableReservations"] === "true") {
+        mealArrayToChange = mealArrayToChange.filter(
+          (item) => item.total_reserved < item.max_reservations
+        );
+      } else if (request.query["availableReservations"] === "false") {
+        mealArrayToChange = mealArrayToChange.filter(
+          (item) =>
+            Number(item.total_reserved) === Number(item.max_reservations)
+        );
+      } else {
+        response
+          .status(400)
+          .json({ error: "Invalid availableReservations param" });
+        return;
+      }
+    }
+
+    if (request.query["maxPrice"]) {
+      if (!isNaN(Number(request.query["maxPrice"]))) {
+        mealArrayToChange = mealArrayToChange.filter(
+          (item) => item.price < Number(request.query["maxPrice"])
+        );
+      } else {
+        response.status(400).json({ error: "Invalid maxPrice param" });
+        return;
+      }
     }
 
     if (request.query["title"]) {
-      queryToMeal = queryToMeal.where(
-        "title",
-        "like",
-        "%" + request.query["title"] + "%"
+      mealArrayToChange = mealArrayToChange.filter((item) =>
+        item.title.toLowerCase().includes(request.query["title"].toLowerCase())
       );
     }
 
     if (request.query["createdAfter"]) {
       const createdAfter = new Date(request.query["createdAfter"]);
       if (createdAfter != "Invalid Date") {
-        queryToMeal = queryToMeal.where("created_date", ">", createdAfter);
+        mealArrayToChange = mealArrayToChange.filter((item) => {
+          const itemDate = new Date(item.createdAt);
+          return itemDate > createdAfter;
+        });
       } else {
-        response.status(400).json({ error: "Invalid date value" });
+        response.status(400).json({ error: "Invalid date param" });
         return;
       }
     }
 
-    if (request.query["limit"] && !isNaN(Number(request.query["limit"]))) {
-      queryToMeal = queryToMeal.limit(request.query["limit"]);
+    if (request.query["limit"]) {
+      if (!isNaN(Number(request.query["limit"]))) {
+        mealArrayToChange = mealArrayToChange.slice(0, request.query["limit"]);
+      } else {
+        response.status(400).json({ error: "Invalid limit param" });
+        return;
+      }
     }
 
-    const allMeals = await queryToMeal.select(
-      "meal.title",
-      "meal.description",
-      "meal.location",
-      "meal.when_time",
-      "meal.max_reservations",
-      "meal.price",
-      "meal.created_date"
-    );
-    if (allMeals.length === 0) {
+    if (mealArrayToChange.length === 0) {
       response.status(404).json({ message: "Meals not found" });
     } else {
-      response.json(allMeals);
+      response.json(mealArrayToChange);
     }
   } catch (error) {
     throw error;
